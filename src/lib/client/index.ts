91e894daf3f2d08600f9ae7b7e58699ab3e5a027
teakzc <tanitinpholsuwa@gmail.com>
2026-05-02 08:58:05 +0700

    the purge

--- src/lib/client/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./core";
-export * from "./render";
-export * from "./atoms";
--- src/tests/jest/jest.config.ts
+++
@@ -1,6 +0,0 @@
-import { Config } from "@rbxts/jest";
-
-export = {
-	displayName: "⚡️ Test",
-	testMatch: ["**/*.test"],
-} satisfies Config;
